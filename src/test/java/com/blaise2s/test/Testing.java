1001db73276f87309f97477d5f7429bb6f454d82
Blaise Schaeffer <Blaise.Schaeffer1@ibm.com>
2018-12-14 08:15:54 -0500

    (test): Checking test functionality

---
+++ src/test/java/com/blaise2s/test/Testing.java
@@ -0,0 +1,10 @@
+package com.blaise2s.test;
+
+import junit.framework.TestCase;
+
+public class Testing extends TestCase {
+
+    public void testTesting() {
+        assertEquals("Hello", "Hello");
+    }
+}
